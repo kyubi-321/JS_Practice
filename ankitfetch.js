1bb75a2a86e5f69c1f5e8a51625361e0f58f2b53
unknown <Ankitbadhani102@gmail.com>
2023-10-27 19:39:54 +0530

    second commit

---
+++ ankitfetch.js
@@ -0,0 +1,11 @@
+
+
+function  takeIt (){
+
+    fetch('https://jsonplaceholder.typicode.com/users/10')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+}
+
+takeIt();
